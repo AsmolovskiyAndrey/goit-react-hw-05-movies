a71aa4011bb474cf6a1c8c6ab85babb87ca6f9cf
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-01-14 21:06:43 +0100

    erste

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { About } from './proba';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template 123
+    <div>
+      <Routes>
+        <Route path="/" element={<About />} />
+      </Routes>
     </div>
   );
 };
